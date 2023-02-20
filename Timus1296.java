f24917b89e3c3e2716ce8a190b546ad35e2b6f03
Konstantin Pinchuk <105445251+Konstant1nov1ch@users.noreply.github.com>
2023-02-20 17:24:09 +0300

    Timus1296

---
+++ Timus1296.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Timus1296 {
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        int count = in.nextInt();
+        System.out.println(MaxSubAr(count, in));
+
+}
+static int MaxSubAr(int count, Scanner in){
+        ArrayList<Integer> MaxSub = new ArrayList<>();
+        MaxSub.add(0);
+        int MaxOfSub = 0;
+        for(int i = 0; i < count; i ++){
+            int tmp = MaxSub.get(MaxSub.size() - 1) + in.nextInt();;
+            if(tmp < 0){tmp = 0;}
+            MaxSub.add(tmp);
+            if(tmp > MaxOfSub){MaxOfSub = tmp;}
+        }
+        return MaxOfSub;
+    }
+}
